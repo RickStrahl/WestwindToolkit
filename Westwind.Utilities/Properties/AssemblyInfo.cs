851a2ec69401ff649475b0b14faf1ea67b19149e
Rick Strahl <rstrahl@west-wind.com>
2013-03-01 22:08:53 -1000

    Project housekeeping.

--- Westwind.Utilities/Properties/AssemblyInfo.cs
+++ Westwind.Utilities/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Westwind.Utilities")]
-[assembly: AssemblyCopyright("Copyright West Wind Technologies, 2006 - 2012")]
+[assembly: AssemblyCopyright("Copyright West Wind Technologies, 2006 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.25.0.0")]
-[assembly: AssemblyFileVersion("1.25.0.0")]
+[assembly: AssemblyVersion("1.99.0.0")]
+[assembly: AssemblyFileVersion("1.99.0.0")]
