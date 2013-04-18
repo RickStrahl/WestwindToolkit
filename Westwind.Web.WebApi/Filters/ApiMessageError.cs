8dea959f6aaa394cc5989d86e921d1bd0645e611
Rick Strahl <rstrahl@west-wind.com>
2013-04-18 12:23:21 -1000

    Updates to ApiErrorMessage for error reporting. Minor housekeeping.

--- Westwind.Web.WebApi/Filters/ApiMessageError.cs
+++ Westwind.Web.WebApi/Filters/ApiMessageError.cs
@@ -7,20 +7,41 @@ namespace Westwind.Web.WebApi
 {
     /// <summary>
     /// Class that represents an error returned to
-    /// the client. Can be explicitly returned or
-    /// as part of the UnhandledExceptionFilter
+    /// the client caller. Can be explicitly returned or
+    /// as part of the UnhandledExceptionFilter.
     /// </summary>
     public class ApiMessageError
     {
+        /// <summary>
+        /// The text message for the errors
+        /// </summary>
         public string message { get; set; }
+
+        /// <summary>
+        /// Flag value that indicates to the client that this
+        /// is an error response
+        /// </summary>
         public bool isCallbackError { get; set; }
+
+        /// <summary>
+        /// An optional list of errors that can be set on the
+        /// error object. Automatically set when passing in 
+        /// a model dictionary with errors.
+        /// </summary>
         public List<string> errors { get; set; }
 
+        /// <summary>
+        /// Default constructor creates empty error object
+        /// </summary>
         public ApiMessageError()
             : this(string.Empty)
         {
         }
 
+        /// <summary>
+        /// Pass in a message string for the exception.
+        /// </summary>
+        /// <param name="errorMessage"></param>
         public ApiMessageError(string errorMessage)
         {
             isCallbackError = true;
@@ -28,6 +49,22 @@ namespace Westwind.Web.WebApi
             message = errorMessage;
         }
 
+        /// <summary>
+        /// Pass in an exception and pick up the message.
+        /// </summary>
+        /// <param name="ex"></param>
+        public ApiMessageError(Exception ex)
+        {
+            isCallbackError = true;
+            errors = new List<string>();
+            message = ex.Message;            
+        }
+
+        /// <summary>
+        /// Pass in a modelState dictionary to create a list of
+        /// binding errors from the API error message
+        /// </summary>
+        /// <param name="modelState"></param>
         public ApiMessageError(ModelStateDictionary modelState)
         {
             isCallbackError = true;
--- Westwind.Web.WebApi/Filters/BasicAuthenticationFilter.cs
+++ Westwind.Web.WebApi/Filters/BasicAuthenticationFilter.cs
@@ -11,134 +11,144 @@ using System.Web.Http.Filters;
 namespace Westwind.Web.WebApi
 {
 
+/// <summary>
+/// Generic Basic Authentication filter that checks for basic authentication
+/// headers and challenges for authentication if no authentication is provided
+/// Sets the Thread Principle with a GenericAuthenticationPrincipal.
+/// 
+/// You can override the OnAuthorize method for custom auth logic that
+/// might be application specific.    
+/// </summary>
+/// <remarks>Always remember that Basic Authentication passes username and passwords
+/// from client to server in plain text, so make sure SSL is used with basic auth
+/// to encode the Authorization header on all requests (not just the login).
+/// </remarks>
+public class BasicAuthenticationFilter : AuthorizationFilterAttribute
+{
+    bool Active = true;
+
+
+    public BasicAuthenticationFilter()
+    { }
+
     /// <summary>
-    /// Generic Basic Authentication filter that checks for basic authentication
-    /// headers and challenges for authentication if no authentication is provided
-    /// Sets the Thread Principle with a GenericAuthenticationPrincipal
-    /// 
-    /// You can override the Authenticate method 
+    /// Overriden constructor to allow explicit disabling of this
+    /// filter's behavior. Pass false to disable (same as no filter
+    /// but declarative)
     /// </summary>
-    public class BasicAuthenticationFilter : AuthorizationFilterAttribute
+    /// <param name="notActive"></param>
+    public BasicAuthenticationFilter(bool notActive)
     {
-        bool Active = true;
+        Active = false;
+    }
 
-        public BasicAuthenticationFilter()
+
+    /// <summary>
+    /// Override to Web API filter method to handle Basic Auth check
+    /// </summary>
+    /// <param name="actionContext"></param>
+    public override void OnAuthorization(HttpActionContext actionContext)
+    {
+        if (Active)
         {
-
-        }
-
-        public BasicAuthenticationFilter(bool nonValidated)
-        {
-            Active = false;
-        }
-
-
-        /// <summary>
-        /// Override to Web API filter method to handle Basic Auth check
-        /// </summary>
-        /// <param name="actionContext"></param>
-        public override void OnAuthorization(HttpActionContext actionContext)
-        {
-            if (Active)
-            {
-                var credentials = ParseAuthorizationHeader(actionContext);
-                if (credentials == null)
-                {
-                    Challenge(actionContext);
-                    return;
-                }
-
-
-                if (!OnAuthorizeUser(credentials.Username, credentials.Password, actionContext))
-                {
-                    Challenge(actionContext);
-                    return;
-                }
-            }
-
-            base.OnAuthorization(actionContext);
-        }
-
-        /// <summary>
-        /// Base implementation for user authentication - you probably will
-        /// want to override this method for application specific logic.
-        /// 
-        /// The base implementation merely checks for username and password
-        /// present and set the Thread principal.
-        /// 
-        /// Override this method if you want to customize Authentication
-        /// and store user data as needed in a Thread Principle or other
-        /// Request specific storage.
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <param name="actionContext"></param>
-        /// <returns></returns>
-        protected virtual bool OnAuthorizeUser(string username, string password, HttpActionContext actionContext)
-        {
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-                return false;
-
-            var principal = new GenericPrincipal(new GenericIdentity(username, "Basic"), null);
-            
-            Thread.CurrentPrincipal = principal;
-
-            // inside of ASP.NET this is requried!
-            //if (HttpContext.Current != null)
-            //    HttpContext.Current.User = principal;
-
-            return true;
-        }
-
-        /// <summary>
-        /// Parses the Authorization header and creates user credentials
-        /// </summary>
-        /// <param name="actionContext"></param>
-        protected virtual BasicAuthCredentials ParseAuthorizationHeader(HttpActionContext actionContext)
-        {
-            string authHeader = null;
-            var auth = actionContext.Request.Headers.Authorization;
-            if (auth != null && auth.Scheme == "Basic")
-                authHeader = auth.Parameter;
-
-            if (string.IsNullOrEmpty(authHeader))
-                return null;
-
-            authHeader = Encoding.Default.GetString(Convert.FromBase64String(authHeader));
-
-            var tokens = authHeader.Split(':');
-            if (tokens.Length < 2)
+            var credentials = ParseAuthorizationHeader(actionContext);
+            if (credentials == null)
             {
                 Challenge(actionContext);
-                return null;
+                return;
             }
 
-            return new BasicAuthCredentials()
+
+            if (!OnAuthorizeUser(credentials.Username, credentials.Password, actionContext))
             {
-                Username = tokens[0],
-                Password = tokens[1]
-            };
+                Challenge(actionContext);
+                return;
+            }
         }
 
-
-        /// <summary>
-        /// Send the Authentication Challenge request
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="actionContext"></param>
-        void Challenge(HttpActionContext actionContext)
-        {
-            var host = actionContext.Request.RequestUri.DnsSafeHost;
-            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
-            actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", host));
-        }
-
+        base.OnAuthorization(actionContext);
     }
 
-
-    public class BasicAuthCredentials
+    /// <summary>
+    /// Base implementation for user authentication - you probably will
+    /// want to override this method for application specific logic.
+    /// 
+    /// The base implementation merely checks for username and password
+    /// present and set the Thread principal.
+    /// 
+    /// Override this method if you want to customize Authentication
+    /// and store user data as needed in a Thread Principle or other
+    /// Request specific storage.
+    /// </summary>
+    /// <param name="username"></param>
+    /// <param name="password"></param>
+    /// <param name="actionContext"></param>
+    /// <returns></returns>
+    protected virtual bool OnAuthorizeUser(string username, string password, HttpActionContext actionContext)
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            return false;
+
+        var principal = new GenericPrincipal(new GenericIdentity(username, "Basic"), null);
+            
+        Thread.CurrentPrincipal = principal;
+
+        // inside of ASP.NET this is requried!
+        //if (HttpContext.Current != null)
+        //    HttpContext.Current.User = principal;
+
+        return true;
     }
+
+    /// <summary>
+    /// Parses the Authorization header and creates user credentials
+    /// </summary>
+    /// <param name="actionContext"></param>
+    protected virtual BasicAuthCredentials ParseAuthorizationHeader(HttpActionContext actionContext)
+    {
+        string authHeader = null;
+        var auth = actionContext.Request.Headers.Authorization;
+        if (auth != null && auth.Scheme == "Basic")
+            authHeader = auth.Parameter;
+
+        if (string.IsNullOrEmpty(authHeader))
+            return null;
+
+        authHeader = Encoding.Default.GetString(Convert.FromBase64String(authHeader));
+
+        var tokens = authHeader.Split(':');
+        if (tokens.Length < 2)
+        {
+            Challenge(actionContext);
+            return null;
+        }
+
+        return new BasicAuthCredentials()
+        {
+            Username = tokens[0],
+            Password = tokens[1]
+        };
+    }
+
+
+    /// <summary>
+    /// Send the Authentication Challenge request
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="actionContext"></param>
+    void Challenge(HttpActionContext actionContext)
+    {
+        var host = actionContext.Request.RequestUri.DnsSafeHost;
+        actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+        actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", host));
+    }
+
+}
+
+
+public class BasicAuthCredentials
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
 }
