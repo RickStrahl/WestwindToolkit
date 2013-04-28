ae25b837850f3a302b94dec5da182ca4795d706f
Rick Strahl <rstrahl@west-wind.com>
2013-04-28 00:03:56 -1000

    Add Web sample project base project.

---
+++ Samples/WestwindToolkitMvcWeb/Controllers/UtilitiesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Westwind.Web;
+
+namespace WestwindToolkitMvcWeb.Controllers
+{
+    public class UtilitiesController : Controller
+    {
+        //
+        // GET: /Utilities/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ScriptVariables()
+        {
+    
+
+            return View();
+        }
+
+    }
+}
--- Samples/WestwindToolkitMvcWeb/_App_Code/App_Start/BundleConfig.cs
+++ Samples/WestwindToolkitMvcWeb/_App_Code/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace WestwindToolkitMvcWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/ww.jquery.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
