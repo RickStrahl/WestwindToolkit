b149f198b2140752728cc9db1b47e4d30a6e8cec
Rick Strahl <rstrahl@west-wind.com>
2014-09-06 19:28:48 -0700

    Add UrlEncodingParser to provide Query String and Form Data Parsing for reading and writing of values.

---
+++ Tests/Westwind.Utilities.Test/UrlEncodingParserTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Westwind.Utilities;
+
+namespace Westwind.Utilities.Tests
+{
+    /// <summary>
+    /// Summary description for StringUtilsTests
+    /// </summary>
+    [TestClass]
+    public class UrlEncodingParserTests
+    {
+        [TestMethod]
+        public void QueryStringTest()
+        {
+            string str = "http://mysite.com/page1?id=3123&format=json&action=edit&text=It's%20a%20brave%20new%20world!";
+
+            var query = new UrlEncodingParser(str);
+
+            Assert.IsTrue(query["id"] == "3123");
+            Assert.IsTrue(query["format"] == "json", "wrong format " + query["format"]);
+            Assert.IsTrue(query["action"] == "edit");
+
+            Console.WriteLine(query["text"]);
+            // It's a brave new world!
+
+            query["id"] = "4123";
+            query["format"] = "xml";
+            query["name"] = "<< It's a brave new world!";
+
+            var url = query.Write();
+
+            Console.WriteLine(url);
+            //http://mysite.com/page1?id=4123&format=xml&action=edit&
+            //text=It's%20a%20brave%20new%20world!&name=%3C%3C%20It's%20a%20brave%20new%20world!
+        }
+
+        [TestMethod]
+        public void QueryStringMultipleTest()
+        {
+            string str = "http://mysite.com/page1?id=3123&format=json&format=xml";
+
+            var query = new UrlEncodingParser(str);
+
+            Assert.IsTrue(query["id"] == "3123");
+            Assert.IsTrue(query["format"] == "json,xml", "wrong format " + query["format"]);            
+
+            // multiple format strings
+            string[] formats = query.GetValues("format");
+            Assert.IsTrue(formats.Length == 2);
+
+            query.SetValues("multiple", new[]
+    {
+        "1",
+        "2",
+        "3"
+    });
+
+            var url = query.Write();
+
+            Console.WriteLine(url);
+
+            Assert.IsTrue(url ==
+                            "http://mysite.com/page1?id=3123&format=json&format=xml&multiple=1&multiple=2&multiple=3");
+
+        }
+
+        [TestMethod]
+        public void WriteUrlTest()
+        {
+            // URL only
+            string url = "http://test.com/page";
+
+            var query = new UrlEncodingParser(url);
+            query["id"] = "321312";
+            query["name"] = "rick";
+
+            url = query.Write();            
+            Console.WriteLine(url);
+
+            Assert.IsTrue(url.Contains("name="));
+            Assert.IsTrue(url.Contains("http://"));
+
+            // URL with ? but no query
+            url = "http://test.com/page?";
+
+            query = new UrlEncodingParser(url);
+            query["id"] = "321312";
+            query["name"] = "rick";
+
+            url = query.Write();
+            Console.WriteLine(url);
+
+            Assert.IsTrue(url.Contains("name="));
+
+
+            // URL with query
+            url = "http://test.com/page?q=search";
+
+            query = new UrlEncodingParser(url);
+            query["id"] = "321312";
+            query["name"] = "rick";
+
+            url = query.Write();
+            Console.WriteLine(url);
+
+            Assert.IsTrue(url.Contains("name="));
+            Assert.IsTrue(url.Contains("http://"));
+
+
+            // Raw query data
+            url = "q=search&name=james";
+
+            query = new UrlEncodingParser(url);
+            query["id"] = "321312";
+            query["name"] = "rick";
+
+            url = query.Write();
+            Console.WriteLine(url);
+
+            Assert.IsTrue(url.Contains("name="));
+            Assert.IsTrue(!url.Contains("http://"));
+
+
+            // No data at all
+            url = null;
+
+            query = new UrlEncodingParser();
+            query["id"] = "321312";
+            query["name"] = "rick";
+
+            url = query.Write();
+            Console.WriteLine(url);
+
+            Assert.IsTrue(url.Contains("name="));
+            Assert.IsTrue(!url.Contains("http://"));
+        }
+    }
+}
---
+++ Westwind.Utilities/SupportClasses/UrlEncodingParser.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+
+namespace Westwind.Utilities
+{
+    /// <summary>
+    /// A query string or UrlEncoded form parser and editor 
+    /// class that allows reading and writing of urlencoded
+    /// key value pairs used for query string and HTTP 
+    /// form data.
+    /// 
+    /// Useful for parsing and editing querystrings inside
+    /// of non-Web code that doesn't have easy access to
+    /// the HttpUtility class.                
+    /// </summary>
+    /// <remarks>
+    /// Supports multiple values per key
+    /// </remarks>
+    public class UrlEncodingParser
+    {
+        /// <summary>
+        /// Internally holds parsed or added query string values
+        /// </summary>
+        public NameValueCollection Values { get; set; }
+
+        /// <summary>
+        ///  Holds the original Url that was assigned
+        /// </summary>
+        private string Url { get; set; }
+
+        /// <summary>
+        /// Always pass in a UrlEncoded data or a URL to parse from
+        /// unless you are creating a new one from scratch.
+        /// </summary>
+        /// <param name="queryStringOrUrl">
+        /// Pass a query string or raw Form data, or a full URL.
+        /// If a URL is parsed the part prior to the ? is stripped
+        /// but saved. Then when you write the original URL is 
+        /// re-written with the new query string.
+        /// </param>
+        public UrlEncodingParser(string queryStringOrUrl = null)
+        {
+            Values = new NameValueCollection();
+            Url = string.Empty;
+
+            if (!string.IsNullOrEmpty(queryStringOrUrl))
+            {
+                Parse(queryStringOrUrl);
+            }
+        }
+
+        /// <summary>
+        /// Indexer that allows access to the query string
+        /// values.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string this[string key]
+        {
+            get { return Values[key]; }
+            set { Values[key] = value; }
+        }
+
+        /// <summary>
+        /// Returns a 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string[] GetValues(string key)
+        {
+            return Values.GetValues(key);
+        }
+
+        /// <summary>
+        /// Assigns multiple values to the same key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="values"></param>
+        public void SetValues(string key, IEnumerable<string> values)
+        {
+            foreach (var val in values)
+                Values.Add(key, val);
+        }
+
+        /// <summary>
+        /// Parses the query string into the internal dictionary
+        /// and optionally also returns this dictionary
+        /// </summary>
+        /// <param name="query">
+        /// Query string key value pairs or a full URL. If URL is
+        /// passed the URL is re-written in Write operation
+        /// </param>
+        /// <returns></returns>
+        public NameValueCollection Parse(string query)
+        {
+            if (query.Contains("//"))
+                Url = query;
+
+            if (query == null)
+                Values = new NameValueCollection();
+            else
+            {
+                int index = query.IndexOf('?');
+                if (index > -1)
+                {
+                    if (query.Length >= index + 1)
+                        query = query.Substring(index + 1);
+                }
+
+                var pairs = query.Split('&');
+                foreach (var pair in pairs)
+                {
+                    int index2 = pair.IndexOf('=');
+                    if (index2 > 0)
+                    {
+                        Values.Add(pair.Substring(0, index2), pair.Substring(index2 + 1));
+                    }
+                }
+            }
+
+            return Values;
+        }
+
+        /// <summary>
+        /// Writes out the urlencoded data/query string or full URL based 
+        /// on the internally set values.
+        /// </summary>
+        /// <returns>urlencoded data or url</returns>
+        public string Write()
+        {
+            string query = string.Empty;
+            foreach (string key in Values.Keys)
+            {
+                string[] values = Values.GetValues(key);
+                foreach (var val in values)
+                {
+                    query += key + "=" + Uri.EscapeUriString(val) + "&";
+                }
+            }
+            query = query.Trim('&');
+
+            if (!string.IsNullOrEmpty(Url))
+            {
+                if (Url.Contains("?"))
+                    query = Url.Substring(0, Url.IndexOf('?') + 1) + query;
+                else
+                    query = Url + "?" + query;
+            }
+
+            return query;
+        }
+    }
+}
