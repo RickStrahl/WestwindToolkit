d2021ccccb4c54a81ff332b279d643d9c7c388bd
Rick Strahl <rstrahl@west-wind.com>
2016-02-03 16:31:01 -1000

    Update Sample Libraries and Migrations

--- Samples/AlbumViewer/AlbumViewerBusiness/Migrations/201601140110091_Initial.Designer.cs
+++ Samples/AlbumViewer/AlbumViewerBusiness/Migrations/201601140110091_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace AlbumViewerBusiness.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201601130055407_initial"; }
+            get { return "201601140110091_Initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ Samples/AlbumViewer/AlbumViewerBusiness/Migrations/201601140110091_Initial.cs
@@ -0,0 +1,68 @@
+    namespace AlbumViewerBusiness.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+public override void Up()
+{
+
+    CreateTable(
+        "dbo.Albums",
+        c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                Title = c.String(),
+                Description = c.String(),
+                Year = c.Int(nullable: false),
+                ImageUrl = c.String(),
+                AmazonUrl = c.String(),
+                SpotifyUrl = c.String(),
+                ArtistId = c.Int(),
+            })
+        .PrimaryKey(t => t.Id)
+        .ForeignKey("dbo.Artists", t => t.ArtistId)
+        .Index(t => t.ArtistId);
+            
+    CreateTable(
+        "dbo.Artists",
+        c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                ArtistName = c.String(maxLength: 128),
+                Description = c.String(),
+                ImageUrl = c.String(maxLength: 256),
+                AmazonUrl = c.String(maxLength: 256),
+            })
+        .PrimaryKey(t => t.Id);
+            
+    CreateTable(
+        "dbo.Tracks",
+        c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                AlbumId = c.Int(),
+                SongName = c.String(maxLength: 128),
+                Length = c.String(maxLength: 10),
+                Bytes = c.Int(nullable: false),
+                UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+            })
+        .PrimaryKey(t => t.Id)
+        .ForeignKey("dbo.Albums", t => t.AlbumId)
+        .Index(t => t.AlbumId);
+            
+}
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tracks", "AlbumId", "dbo.Albums");
+            DropForeignKey("dbo.Albums", "ArtistId", "dbo.Artists");
+            DropIndex("dbo.Tracks", new[] { "AlbumId" });
+            DropIndex("dbo.Albums", new[] { "ArtistId" });
+            DropTable("dbo.Tracks");
+            DropTable("dbo.Artists");
+            DropTable("dbo.Albums");
+        }
+    }
+}
