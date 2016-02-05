6a2d00818b98ff965f412bdd7d828da68dc93a8d
Rick Strahl <rstrahl@west-wind.com>
2016-02-04 17:02:03 -1000

    Add additional overloads to CallbackException Add new CallbackError class that mimick CallbackException but isn't an exception and thus serializes more easily and allows setting of the Message and StackTrace properties.

--- Westwind.Web/CallbackHandler/CallbackHandler/CallbackException.cs
+++ Westwind.Web/CallbackHandler/CallbackHandler/CallbackException.cs
@@ -19,15 +19,61 @@ namespace Westwind.Web
             StatusCode = 500;
         }
 
+        public CallbackException(string message, Exception innerException) : base(message, innerException)
+        {            
+        }
+
         public CallbackException(string message, int statusCode = 500) : base(message)
-        {
-            if (message == null)
-                message = string.Empty;
-            
+        {            
             StatusCode = statusCode;
         }
 
         public bool IsError {get; set; }
+
+        /// <summary>
+        /// Additional Error Detail to assign to this error instance
+        /// </summary>
+        public string ErrorDetail { get; set; }
+
         public int StatusCode { get; set; }
+    }
+
+
+    /// <summary>
+    /// Mimicks the CallbackException class except this is not based on an Exception.
+    /// Ideal for passing back as an error result from a failed serialization request
+    /// in an API or other service interface.
+    /// </summary>
+    public class CallbackError 
+    {
+        public CallbackError()
+        {
+            IsError = true;
+            StatusCode = 500;
+        }
+
+        public CallbackError(string message, int statusCode = 500) 
+        {
+            if (message == null)
+                message = string.Empty;
+
+            IsError = true;
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        public bool IsError { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Additional Error Detail to assign to this error instance
+        /// </summary>
+        public string ErrorDetail { get; set; }
+
+
+        public string StackTrace { get; set; }
     }
 }
